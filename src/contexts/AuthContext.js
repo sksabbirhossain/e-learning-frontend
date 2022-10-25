0b33ea4e2ce26590b6a7112c7c97f4dce9b6a12f
sksabbirhossain <sksabbirhossain4@gmail.com>
2022-10-26 04:47:31 +0600

    created a authcontext for user management

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,69 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+
+import React, { createContext, useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import "../firebase/firebase";
+
+const AuthContext = createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [loading, setLoading] = useState(true);
+  const [currentUser, setCurrentUser] = useState();
+
+  const auth = getAuth();
+
+  useEffect(() => {
+    const unsubcrive = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubcrive;
+  });
+
+  // create a user
+  const userSignup = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  // login user
+  const userLogin = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  // user logOut
+  const logOut = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        console.log("logout");
+        toast.success("LogOut successful");
+      })
+      .catch((error) => {
+        // An error happened.
+        console.log(error);
+        toast.error("something wrong!!");
+      });
+  };
+
+  const value = {
+    currentUser,
+    userSignup,
+    userLogin,
+    logOut,
+  };
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+}
