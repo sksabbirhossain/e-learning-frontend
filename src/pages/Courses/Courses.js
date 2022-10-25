c6c88f41b5324ab5c152ef264c1bba53ad01ba54
sksabbirhossain <sksabbirhossain4@gmail.com>
2022-10-26 03:17:31 +0600

    showing course category data in courses page

--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -1,23 +1,39 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import Sidebar from "../../components/Sidebar/Sidebar";
-import "./Courses.css"
+import "./Courses.css";
 
 const Courses = () => {
-  
+  const [courseCategory, setCourseCategory] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
+    fetch(`/api/courses/${id}`)
+      .then((res) => res.json())
+      .then((data) => setCourseCategory(data));
+  }, [id]);
+
   return (
     <div className="container">
       <h3 className="text-center mt-2 mt-md-3">Our Courses </h3>
       <div className="row">
         <div className="col-md-3">
-          <Sidebar/>
+          <Sidebar />
         </div>
         <div className="col-md-8">
-          <div className="row">
-            <div className="col-sm-4 col-lg-3">
-              <div className="card mb-3">
-                <h2>hello</h2>
+          <div className="row mt-3 mt-md-0">
+            {courseCategory.map((items) => (
+              <div className="col-sm-6 col-lg-4">
+                <div className="card mb-3">
+                  <img src={items.img} className="card-img" alt="..." />
+                  <div className="card-body">
+                    <p className="card-title">{items.title}</p>
+                    <a href="/" className="btn btn-success w-100">
+                      Learn more
+                    </a>
+                  </div>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
