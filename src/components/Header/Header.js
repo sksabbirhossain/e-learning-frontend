36177e7bd5bcc73f2ce5dfa9923e57858602be55
sksabbirhossain <sksabbirhossain4@gmail.com>
2022-10-25 17:19:57 +0600

    created a tooltips to showing username

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,74 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from 'react-router-dom';
-import { FaSun , FaMoon} from "react-icons/fa";
-import { useState } from 'react';
+import Tippy from "@tippyjs/react";
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import "tippy.js/dist/tippy.css";
+import "./Header.css";
 
 const Header = () => {
-    const [theme, setTheme] = useState(false)
-    return (
-        <Navbar bg="light" expand="lg">
-      <Container >
+  const [theme, setTheme] = useState(false);
+  const currentUser = true;
+  const userName = "sk sabbir hossain"
+  return (
+    <Navbar expand="lg">
+      <Container>
         <Navbar.Brand href="/">Navbar scroll</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="ms-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
+            className="ms-auto my-2 my-lg-0 align-items-center"
+            style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <Nav.Link ><NavLink to="/" end>Courses</NavLink></Nav.Link>
-            <Nav.Link ><NavLink to="/faq" >FAQ</NavLink></Nav.Link>
-            <Nav.Link ><NavLink to="/blog" >Blog</NavLink></Nav.Link>
-            <Nav.Link ><NavLink to="/login" >Login</NavLink></Nav.Link>
-            <Nav.Link >
-                <p onClick={() => setTheme(!theme)}>{ theme ? <><FaMoon /> <small>Dark</small> </>: <><FaSun /> <small>Light</small> </> }</p>
+            <Nav.Link>
+              <NavLink to="/" end>
+                Courses
+              </NavLink>
+            </Nav.Link>
+            <Nav.Link>
+              <NavLink to="/faq">FAQ</NavLink>
+            </Nav.Link>
+            <Nav.Link>
+              <NavLink to="/blog">Blog</NavLink>
+            </Nav.Link>
+            <Nav.Link>
+              {currentUser ? (
+                <>
+                  <Tippy content={userName}>
+                    <img
+                      src="https://freesvg.org/img/abstract-user-flat-4.png"
+                      alt=""
+                      className="user-logo"
+                    />
+                  </Tippy>
+                </>
+              ) : (
+                <>
+                  <NavLink to="/login">Login</NavLink>
+                </>
+              )}
+            </Nav.Link>
+            <Nav.Link>
+              <span onClick={() => setTheme(!theme)}>
+                {theme ? (
+                  <>
+                    <FaMoon /> <small>Dark</small>{" "}
+                  </>
+                ) : (
+                  <>
+                    <FaSun /> <small>Light</small>{" "}
+                  </>
+                )}
+              </span>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
-    );
+  );
 };
 
 export default Header;
